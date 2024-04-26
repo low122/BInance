9815338da78241a7ea229a5999cd3a90cd2eb3c4
lowjiatzin <jlow03@students.ubc.ca>
2024-04-25 20:58:24 -0700

    Create Class for Trade

--- Binance/binance-api.py
+++ Binance/binance-api.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from dotenv import load_dotenv
-import os
 from binance.client import Client
 import requests
+import os
 import json
 import websocket
 from websocket import WebSocketApp
@@ -10,38 +10,46 @@ from datetime import datetime
 
 load_dotenv()
 
-api_key = 'NQbQTdz3KpUDvbGau9ranZUS7LxogPwuKERQ0Lf822OccIwzBdWXdzhSah9UApTG'
-api_secret = 'XSbXZPnIl4K6lolkjc9dN0PCvRyS9JFgJCKx0bsXQUhV6PnqRP77bOh1eS7UNM3e'
+API_KEY = os.getenv('BINANCE_API_KEY')
+API_SECRET = os.getenv('BINANCE_API_SECRET')
+client = Client(api_key=API_KEY, api_secret=API_SECRET, testnet=True)
 
-client = Client(api_key=api_key, api_secret=api_secret, testnet=True)
+class BinanceTrade:
+    def __init__(self, symbol):
+        self.symbol = symbol.lower()
+        self.trade_socket = f'wss://stream.binance.com:9443/ws/{self.symbol}@trade'
+        self.kline_socket = f'wss://nbstream.binance.com/eoptions/ws/{self.symbol}@kline_1m'
+        self.ws = None
 
-socket = 'wss://stream.binance.com:9443/ws/btcusdt@trade'
-klineSocket = 'wss://nbstream.binance.com/eoptions/ws/btcusdt/@kline_1m'
+    def on_message(self, ws, message):
+        json_message = json.loads(message)
+        curr_event_time = json_message['E'] / 1000
+        event_time = datetime.utcfromtimestamp(curr_event_time)
 
-def on_message(ws, message):
-    jsonMessage = json.loads(message)
-    currEventTime = jsonMessage['E'] / 1000
-    event_time = datetime.utcfromtimestamp(currEventTime)
+        curr_mkt_price = json_message['p']
+        print(f'Market Price: {curr_mkt_price} -- {event_time}')
 
-    currMktPrice = jsonMessage['p']
-    print(f'Market Price: {currMktPrice} -- {event_time}')
+    def comparePrice():
+        pass
 
-def on_error(ws, error):
-    print("Error encountered:", error)
+    def on_error(self, ws, error):
+        print("Error encountered:", error)
 
-def on_close(ws, close_status_code, close_msg):
-    print('------- CLOSED --------', close_status_code, close_msg)
+    def on_close(self, ws, close_status_code, close_msg):
+        print('------- CLOSED --------', close_status_code, close_msg)
 
-def on_open(ws):
-    print("Opened Connections")
-    
+    def on_open(self, ws):
+        print("Opened Connection")
 
+    def run(self):
+        self.ws = WebSocketApp(self.trade_socket,
+                               on_open=self.on_open,
+                               on_message=self.on_message,
+                               on_error=self.on_error,
+                               on_close=self.on_close)
+        self.ws.run_forever()
 
 if __name__ == '__main__':
-    ws = WebSocketApp(url=socket,
-                        on_open=on_open,
-                        on_message=on_message,
-                        on_error=on_error,
-                        on_close=on_close)
-    
-    ws.run_forever()
+    symbol = 'btcusdt'  # Example symbol, can be replaced with any other
+    ws_client = BinanceTrade(symbol)
+    ws_client.run()
